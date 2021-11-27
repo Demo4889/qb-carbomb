53ad9d60ebd751dded5f5892b86ba0f151dd9ed2
Demo <51279632+Demo4889@users.noreply.github.com>
2021-11-26 22:23:49 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,50 +1,48 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local timer = 0
 local armedVeh
-local sleep = true
 
 RegisterNetEvent('qb-carbomb:CheckVehicle', function()
     local ped = PlayerPedId()
     local pCoords = GetEntityCoords(ped)
     local veh = QBCore.Functions.GetClosestVehicle(pCoords)
     local vCoords = GetEntityCoords(veh)
-    local dist = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, vCoords.x, vCoords.y, vCoords.z, true)
+    local dist = GetDistanceBetweenCoords(pCoords.x, pCoords.y, pCoords.z, vCoords.x, vCoords.y, vCoords.z, true)
     local animDict = "anim@amb@business@weed@weed_inspecting_lo_med_hi@"
     local anim = "weed_spraybottle_crouch_base_inspector"
 
     if not IsPedInAnyVehicle(ped, false) then
         if veh and (dist < 4.0) then
-            loadAnimDict(animDict)
-            Wait(1000)
-            TaskPlayAnim(ped, animDict, anim, 3.0, 1.0, -1, 0, 1, 0, 0, 0)
             QBCore.Functions.Progressbar("carbomb_arming", "Arming Device...", Config.ArmingTime, false, true,{
                 disableMovement = false,
                 disableCarMovement = false,
                 disableMouse = false,
                 disableCombat = true,
             }, {
-                animDict = "anim@amb@business@weed@weed_inspecting_lo_med_hi@"
+                animDict = "anim@amb@business@weed@weed_inspecting_lo_med_hi@",
                 anim = "weed_spraybottle_crouch_base_inspector"
             }, {}, {}, function()
                 ClearPedTasksImmediately(ped)
-                TriggerServerEvent('qb-carbomb:RemoveBombFromInv')
                 for k,v in pairs(Config.Detonation) do
-                    if v.type == 0 then
-                        QBCore.Functions.Notify('The device will explode in'..v.detonation..' seconds', 'success')
-                        RunTimer(veh)
-                    elseif v.type == 1 then
-                        QBCore.Functions.Notify('The device will explode when the vehicle reaches '..v.speed..Config.Speed, 'success')
-                        armedVeh = veh
-                    elseif v.type == 2 then
-                        QBCore.Functions.Notify('Detonate the device by pressing [G]', 'success')
-                        armedVeh = veh
-                    elseif v.type == 3 then
-                        QBCore.Functions.Notify('The device will detonate '..v.detonation..' seconds after someone enters the vehicle', 'success')
-                        armedVeh = veh
-                    else
-                        QBCore.Functions.Notify('The device will explode as soon as someone enters the drivers seat', 'success')
+                    if k == Config.DetonationType then
+                        results = v
+                        if Config.DetonationType == 1 then
+                            QBCore.Functions.Notify('The device will explode in '..results.detonation..' seconds', 'success')
+                            RunTimer(veh, results.detonation)
+                        elseif Config.DetonationType == 2 then
+                            QBCore.Functions.Notify('The device will explode when the vehicle reaches '..results.speed..' '..Config.Speed, 'success')
+                        elseif Config.DetonationType == 3 then
+                            QBCore.Functions.Notify('Detonate the device by pressing [G]', 'success')
+                        elseif Config.DetonationType == 4 then
+                            QBCore.Functions.Notify('The device will detonate '..results.detonation..' seconds after someone enters the vehicle', 'success')
+                        elseif Config.DetonationType == 5 then
+                            QBCore.Functions.Notify('The device will explode as soon as someone enters the drivers seat', 'success')
+                        end
                     end
+                    break
                 end
+                armedVeh = veh
+                TriggerServerEvent('qb-carbomb:RemoveBombFromInv')
             end)
         else
             QBCore.Functions.Notify("You are not near any vehicles", "error")
@@ -57,43 +55,43 @@ end)
 
 CreateThread(function()
     while true do
-        sleep = true
         if armedVeh then
-            sleep = false
             for k,v in pairs(Config.Detonation) do
-                if v.type == 1 and armedVeh then
-                    local speed = GetEntitySpeed(armedVeh)
-                    local SpeedKMH = speed * 3.6
-                    local SpeedMPH = speed * 2.236936
-                    
-                    if Config.Speed == 'MPH' then
-                        if SpeedMPH >= v.speed then
+                if k == Config.DetonationType then
+                    results = v
+                    if Config.DetonationType == 1 and armedVeh then
+                        RunTimer(armedVeh, results.detonation)
+                    elseif Config.DetonationType == 2 and armedVeh then
+                        local speed = GetEntitySpeed(armedVeh)
+                        local SpeedKMH = speed * 3.6
+                        local SpeedMPH = speed * 2.236936
+                        
+                        if Config.Speed == 'MPH' then
+                            if SpeedMPH >= results.speed then
+                                DetonateVehicle(armedVeh)
+                            end
+                        elseif Config.Speed == 'KPH' then
+                            if SpeedKMH >= results.speed then
+                                DetonateVehicle(armedVeh)
+                            end
+                        end
+                    elseif Config.DetonationType == 3 and armedVeh then
+                        if IsControlJustReleased(0, results.key) then
                             DetonateVehicle(armedVeh)
                         end
-                    elseif Config.Speed == 'KPH' then
-                        if SpeedKMH >= v.speed then
+                    elseif Config.DetonationType == 4 and armedVeh then
+                        if not IsVehicleSeatFree(armedVeh, -1)  then
+                            RunTimer(armedVeh, results.detonation)
+                        end
+                    elseif Config.DetonationType == 5 and armedVeh then
+                        if not IsVehicleSeatFree(armedVeh, -1) then
                             DetonateVehicle(armedVeh)
                         end
-                    end
-                elseif v.type == 2 and armedVeh then
-                    if IsControlJustReleased(0, Config.TriggerKey) then
-                        DetonateVehicle(armedVeh)
-                    end
-                elseif v.type == 3 and armedVeh then
-                    if not IsVehicleSeatFree(armedVeh, -1)  then
-                        RunTimer(armedVeh, v.detonation)
-                    end
-                elseif v.type == 4 and armedVeh then
-                    if not IsVehicleSeatFree(armedVeh, -1) then
-                        DetonateVehicle(armedVeh)
                     end
                 end
             end
         end
-
-        if sleep then
-            Wait(1000)
-        end
+        Wait(3)
     end
 end)
 
@@ -104,6 +102,7 @@ function RunTimer(veh, time)
         Wait(1000)
         if timer == 0 then
             DetonateVehicle(veh)
+            armedVeh = nil
         end
     end
 end
