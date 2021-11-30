168c9f80b248dabda25f87c650ca2483f3d3d8ae
Demo <51279632+Demo4889@users.noreply.github.com>
2021-11-29 20:26:06 -0500

    Update server.lua
    Added disarm and inspect vehicle commands, not just for police

--- server.lua
+++ server.lua
@@ -1,7 +1,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem('ied', function(source)
-    TriggerClientEvent("qb-carbomb:CheckVehicle", source)
+    local src = source
+    TriggerClientEvent("qb-carbomb:CheckVehicle", src)
 end)
 
 RegisterServerEvent('qb-carbomb:RemoveBombFromInv')
@@ -12,4 +13,14 @@ AddEventHandler('qb-carbomb:RemoveBombFromInv', function()
     if item.amount > 0 then
         Player.Functions.RemoveItem(Config.Item, 1)
     end
+end)
+
+QBCore.Commands.Add('disarm', 'Disarm a car bomb', {}, false, function(source)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent('qb-carbomb:disarmBomb', source)
+end)
+
+QBCore.Commands.Add('inspect', 'Inspect the vehicle for a bomb', {}, false, function(source)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent('qb-carbomb:inspectBomb', source)
 end)
